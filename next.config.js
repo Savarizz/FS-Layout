f8f5fbcf0d32e9b8ee3d288baa79e324f076af4d
Savarizz <diegosavari3@gmail.com>
2023-02-09 18:31:58 -0300

    add loader

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
-    loader: 'imgix',
-    path: 'https://savari.imgix.net',
+    loader: "akamai",
+    path: "",
   },
 }
 
